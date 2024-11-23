d2501c9b0d5543d47205bb1ed93a3b43407c7c27
Igor <igordyb@gmail.com>
2024-11-23 17:29:43 +0100

    Outlining goals and setting up a basic framework

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 import customtkinter as ctk
 import random
 
-# WHAT THE NUMBER GUESSING APP NEEDS:
-# 1. Input box
-# 2. Function to check if the number input matches the random number, if so outputs the result in console
+# WHAT THE NUMBERGUESSR NEEDS:
+# 1. An input box with a way to store user's input in a variable
+# 2. A function that generates a random number and checks whether the input matches the number.
+#    a) Notify the player whether they guess correctly or not
+#    b) Add a limit on guesses, leading to "Game over" if they exceed it.
+# 3. Feedback mechanism - a function that tells the user either "Too high!" or "Too low!" depending on his guess
+# 4. Difficulty levels - allow the user to choose his difficulty level. E.g. 1-10, 1-50, 1-100 etc.
+# 5. Create and allow the user to change the game's themes
+# 6. Error handling
+# 7. The ability to save progress
+# 8. A good-looking UI with effects/animations
 
 def num_gen(event):
     print("hello")
 
-class App(ctk.CTk):
+# class NumberGuessr(ctk.CTk):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.title("Guess the Number")
+#         self.geometry("360x600")
+#         ctk.set_appearance_mode("dark")
+#         ctk.set_default_color_theme("dark-blue")
+#
+#         self.frame = ctk.CTkFrame(master=self)
+#         self.frame.pack(side="bottom", pady=100)
+#
+#         self.entry = ctk.CTkEntry(master=self.frame, font=("Roboto", 13), width=225)
+#         self.entry.pack(pady=20)
+#         self.entry.bind("<Return>", num_gen)
+
+class NumberGuessr(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("Guess the Number")
-        self.geometry("360x600")
-        ctk.set_appearance_mode("dark")
-        ctk.set_default_color_theme("dark-blue")
+    def gen_random_number(self):
+        pass
 
-        self.frame = ctk.CTkFrame(master=self)
-        self.frame.pack(side="bottom", pady=100)
-        self.frame1 = ctk.CTkFrame(master=self)
-        self.frame1.pack(side="top", pady=40)
+    def check_the_guess(self):
+        pass
 
-        self.label = ctk.CTkLabel(master=self.frame1, text="NUMBERGUESSR", font=("Roboto-Bold", 35))
-        self.label.pack()
+    def start_a_new_round(self):
+        pass
 
-        self.entry = ctk.CTkEntry(master=self.frame, font=("Roboto", 13), width=225)
-        self.entry.pack(pady=20)
-        self.entry.bind("<Return>", num_gen)
+class NumberGuessrUI(ctk.CTk, NumberGuessr):
+    def __init__(self):
+        super().__init__()
 
 
-app = App()
+def main():
+    pass
+
+app = NumberGuessr()
 app.mainloop()
